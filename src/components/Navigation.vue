85ce59e38086880df681559c030debd3d9d8c118
Alexandru Croitoriu <croitoriu.alexandru.code@gmail.com>
2025-07-03 23:52:00 +0300

    save

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -17,7 +17,7 @@ const showMenu = ref(false)
         :class="showMenu ? '-rotate-45 -translate-y-0.5' : 'rotate-0 translate-y-1'"></div>
       </div>
     </button>
-    <div v-if="showMenu" class="absolute top-full mt-2 sm:left-1/2 transform sm:-translate-x-1/2 left-0 right-0 w-xs mx-auto sm:w-fit bg-gray-900 text-gray-200 flex items-center border border-gray-700 w-fit rounded-xl shadow-lg z-10 text-center">
+    <div v-if="showMenu" class="absolute top-full mt-2 sm:left-1/2 transform sm:-translate-x-1/2 left-0 right-0 w-xs mx-auto sm:mx-0 sm:w-fit bg-gray-900 text-gray-200 flex items-center border border-gray-700 w-fit rounded-xl shadow-lg z-10 text-center">
       <nav class="fle flex-col w-full sm:w-fit">
         <!-- <router-link to="/" @click="showMenu = false" class="block text-gray-700 hover:text-blue-600">Home</router-link> -->
         <!-- <router-link to="/schedule" @click="showMenu = false" class="block text-gray-700 hover:text-blue-600">Schedule</router-link> -->
