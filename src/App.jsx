c7fb2ae1d6d3e3a93b4998c66673de20769f1899
Alexandru Croitoriu <croitoriu.alexandru.code@gmail.com>
2025-07-03 12:26:10 +0300

    save

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './assets/tailwind.css'
+
+function App() {
+
+  return (
+    <>
+    <div class="text-lg">text</div>
+    </>
+  )
+}
+
+export default App
